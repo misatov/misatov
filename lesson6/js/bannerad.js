ff1eb523b4c1c716b3fb76c98c1eb4f34cbebe05
misatov <vel20002@byui.edu>
2021-02-16 19:31:13 -0600

    update

---
+++ lesson6/js/bannerad.js
@@ -0,0 +1,10 @@
+
+    var d = new Date();
+    var n = d.getDay();
+    n = 4;
+    if (n == 5){
+        document.getElementById("pan").style.display = "block"; 
+
+    }else {
+        document.getElementById("pan").style.display = "none"; 
+    }
