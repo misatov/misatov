565fca944b3bb8faeb86d530d03c2414e785fcc3
misatov <vel20002@byui.edu>
2021-02-19 00:19:21 -0600

    error fixed

--- lesson7/script.js
+++ lesson7/script.js
@@ -2,7 +2,7 @@ const imageToLoad = document.querySelectorAll("img[data-src]");
 
 // option parameters being set for the IntersectionObserver
 const imgOptions = {
-    threshold: 0,
+    threshold: 1,
     rootMargin: "0px 0px 50px 0px"
 };
 
@@ -20,18 +20,18 @@ if('IntersectionObserver' in window){
                 return;
             }
             else{
-                preloadImage(item.target);
+                loadImages(item.target);
                 imgObserver.unobserve(item.target);
             }
 
         });
     },imgOptions);
 
-    imagesToLoad.forEach((img) =>{
+    imageToLoad.forEach((img) =>{
     imgObserver.observe(img);
 });
 }else{
-    imagesToLoad.forEach((img) =>{
+    imageToLoad.forEach((img) =>{
         loadImages(img);
     });
 }
