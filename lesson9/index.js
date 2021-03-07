61779eb62acd36cdec46a8d674b6b421104a02d4
misatov <vel20002@byui.edu>
2021-03-06 22:02:42 -0600

    push

---
+++ lesson9/index.js
@@ -0,0 +1,18 @@
+//set the JSON source URL
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+// use fetch to obtain a promise for
+
+
+// temporary checking for valid response and data parsing
+
+
+
+//output conditions
+
+
+   
+//use template intervals
+
+
+//select output location//
