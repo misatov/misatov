4a7685c6379605b324a63e95b8e25f539b83a6dd
misatov <vel20002@byui.edu>
2021-03-09 19:12:30 -0600

    add fetch

--- lesson9/js/index.js
+++ lesson9/js/index.js
@@ -1,44 +1,45 @@
 //set the JSON source URL
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
-.then(function (response){
-    return response.json();
-})
-.then(function(jsonObject){
-    const towns = jsonObject['towns'];
-    const three = towns.filter(towns =>(towns.name == "Fish Haven" || towns.name == "Preston" || towns.name == "Soda Springs"));
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        const towns = jsonObject['towns'];
+        const three = towns.filter(towns => (towns.name == "Fish Haven" || towns.name == "Preston" || towns.name == "Soda Springs"));
 
-    // codes ideas from the discussion board of Week 9
+        // codes ideas from the discussion board of Week 9
 
-    for(let i=0; i < three.length; i++){
-        let card = document.createElement('section'); // insert in section tab
-        let heading = document.createElement('h2');
-        
-        let motto = document.createElement('h3');
-        let year = document.createElement('p');
-        let cP = document.createElement('p');
-        let aR = document.createElement('p');
-        let image = document.createElement('img');
-        
-        //output conditions
-        heading.textContent =three[i].name;
-        heading.setAttribute('class','town_name');
-        motto.textContent = three[i].motto;
-        motto.setAttribute('class', 'town_motto');
-        year.textContent = 'Year Founded:' + three[i].yearFounded;
-        cP.textContent = 'Current Population:' + three[i].currentPopulation;
-        aR.textContent = ' Annual Rain Fall:' + three[i].averageRainfall;
-        image.setAttribute('src','images/' + three[i].photo);
-        image.setAttribute('src', three[i].name);
+        for (let i = 0; i < three.length; i++) {
+            let card = document.createElement('section'); // insert in section tab
+            let heading = document.createElement('h2');
 
-        //select output location//
-        card.appendChild(heading);
-        card.appendChild(motto);
-        card.appendChild(year);
-        card.appendChild(cP);
-        card.appendChild(aR);
-        card.appendChild(image);
+            let motto = document.createElement('h3');
+            let year = document.createElement('p');
+            let cP = document.createElement('p');
+            let aR = document.createElement('p');
+            let image = document.createElement('img');
 
-        document.querySelector('div.idahoTown').appendChild(card);
+            //output conditions
+            heading.textContent = three[i].name;
+            heading.setAttribute('class', 'town_name');
+            motto.textContent = three[i].motto;
+            motto.setAttribute('class', 'town_motto');
+            year.textContent = 'Year Founded:' + three[i].yearFounded;
+            cP.textContent = 'Current Population:' + three[i].currentPopulation;
+            aR.textContent = ' Annual Rain Fall:' + three[i].averageRainfall;
+            image.setAttribute('src', 'images/' + three[i].photo);
+            image.setAttribute('src', three[i].name);
 
-    }
-});
+            //select output location//
+            card.appendChild(heading);
+            card.appendChild(motto);
+            card.appendChild(year);
+            card.appendChild(cP);
+            card.appendChild(aR);
+            card.appendChild(image);
+
+            document.querySelector('div.idahoTown').appendChild(card);
+
+        }
+    });
