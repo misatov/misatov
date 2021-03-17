deff0375238a8e77e8415ff1eacf68ddda6a6a95
misatov <vel20002@byui.edu>
2021-03-16 20:43:35 -0600

    change in js

--- lesson10/js/prestonapi.js
+++ lesson10/js/prestonapi.js
@@ -21,26 +21,27 @@ fetch (apiURL2)
     //console.log(jsObject);
  
     const forecast = jsObject['list'];
-    const table = document.querySelector(#forecast);
+    const table = document.querySelector(".forecast");
 
     const forecastfilter = forecast.filter((x) =>
-    x.dt.txt.includes("18:00:00")
+    x.dt_txt.includes("18:00:00")
     );
-    //console.log(forecastfilter);
+    console.log(forecastfilter);
 
     let day = 0;
-    const dayofweek = ['Sun', 'Mon','Tue','Wed','Thu','Fri','Sat'];
-
+    const dayofWeek = ['Sun', 'Mon','Tue','Wed','Thu','Fri','Sat'];
+    dayNumber++;
+    
     //print in table
-    dayofWeek.forEach((filtered => {
+    dayofWeek.forEach((forecastfilter => {
         let tData  = document.createElement('td');
         let tImg = document.createElement('img');
-        let tImgURL = 'https:openweathermap.org/img/w' + `{$filtered.weather[0].icon}` + '.png';
-        let d = `${filtered.weather[0].description}`;
+        let tImgURL = 'https://openweathermap.org/img/w' + `${forecastfilter.weather[0].icon}` + '.png';
+        let d = `${forecastfilter.weather[0].description}`;
 
         tImg.setAttribute("src", tImgURL);
         tImg.setAttribute("alt",d);
-        tData.innerHTML = tableimg + `{$filtered.main.temp.toFixed(0)}`;
+        tData.innerHTML = tableimg + `{$forecastfilter.main.temp.toFixed(0)}`;
         
         table.appendChild(tData);
         console.log(tImgURL);
